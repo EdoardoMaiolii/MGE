4b03d70dd7ec7c5a1051a7f5c20f9dc18dc4ec60
kevin mancini <kevin.mancini@studio.unibo.it>
2020-05-04 17:39:01 +0200

    correct checkstyle

--- src/main/it/unibo/oop/mge/model/FunctionFeatures.java
+++ src/main/it/unibo/oop/mge/model/FunctionFeatures.java
@@ -1,32 +1,33 @@
 package it.unibo.oop.mge.model;
 import java.util.List;
 
-import it.unibo.oop.mge.c3d.geometry.*;
-import it.unibo.oop.mge.libraries.*;
+import it.unibo.oop.mge.c3d.geometry.Segment3D;
+import it.unibo.oop.mge.libraries.PointND;
+
 public interface FunctionFeatures {
-	/**
-	 * 
-	 * @return get the points belong to the domain
-	 */
-	public List<PointND> getPointsInDomain();
-	/**
-	 * 
-	 * @return get the points that not belong to the domain
-	 */
-	public List<PointND> getPointsOutOfDomain();
-	/**
-	 * 
-	 * @return get the Absolute Max of the function in the interval
-	 */
-	public PointND getAbsoluteMax();
-	/**
-	 * 
-	 * @return get the Absolute Min of the function in the interval
-	 */
-	public PointND getAbsoluteMin();
-	/**
-	 * 
-	 * @return get the Poligonal Model of the Function in the interval
-	 */
-	public List<Segment3D> getPolygonalModel();
+    /**
+     * 
+     * @return get the points belong to the domain
+     */
+    List<PointND> getPointsInDomain();
+    /**
+     * 
+     * @return get the points that not belong to the domain
+     */
+    List<PointND> getPointsOutOfDomain();
+    /**
+     * 
+     * @return get the Absolute Max of the function in the interval
+     */
+    PointND getAbsoluteMax();
+    /**
+     * 
+     * @return get the Absolute Min of the function in the interval
+     */
+    PointND getAbsoluteMin();
+    /**
+     * 
+     * @return get the Poligonal Model of the Function in the interval
+     */
+    List<Segment3D> getPolygonalModel();
 }
--- src/main/it/unibo/oop/mge/model/FunctionFeaturesBuilder.java
+++ src/main/it/unibo/oop/mge/model/FunctionFeaturesBuilder.java
@@ -1,38 +1,60 @@
 package it.unibo.oop.mge.model;
 import java.util.Optional;
 
-import it.unibo.oop.mge.function.*;
-import it.unibo.oop.mge.libraries.*;
-public class FunctionFeaturesBuilder {
-	private Optional<AlgebricFunctionImpl<?>> function;
-	private Optional<Pair<Double,Double>> interval;
-	private Optional<Double> rate;
-	
-	private Boolean ready() {
-		return function.isPresent() && interval.isPresent() && rate.isPresent();
-	}
-	private void printError() {
-		System.out.println("Error using FunctionFeaturesBuilder");
-	}
-	
-	public FunctionFeaturesBuilder setFunction(AlgebricFunctionImpl<?> function) {
-			this.function = Optional.of(function);
-			return this;
-	}
-	public FunctionFeaturesBuilder setIntervals(Pair<Double,Double> interval) {
-			this.interval = Optional.of(interval);
-			return this;
-	}
-	public FunctionFeaturesBuilder setRate(Double rate) {
-			this.rate = Optional.of(rate);
-			return this;	
-	}
-	public FunctionFeaturesImpl build() {
-		if (ready())
-			return new FunctionFeaturesImpl(function.get(),interval.get(),rate.get());
-		else {
-			printError();
-			return null;
-		}
-	}
+import it.unibo.oop.mge.function.AlgebricFunctionImpl;
+import it.unibo.oop.mge.libraries.Pair;
+
+public final class FunctionFeaturesBuilder {
+    private Optional<AlgebricFunctionImpl<?>> function;
+    private Optional<Pair<Double, Double>> interval;
+    private Optional<Double> rate;
+    private Boolean builded = false;
+
+    private Boolean ready() {
+        return function.isPresent() && interval.isPresent() && rate.isPresent() && !builded.booleanValue();
+    }
+
+    private void printError() {
+        System.out.println("Error using FunctionFeaturesBuilder");
+    }
+
+    public FunctionFeaturesBuilder setFunction(final AlgebricFunctionImpl<?> function) {
+        if (!builded.booleanValue() && this.function.isEmpty()) {
+            this.function = Optional.of(function);
+            return this;
+        } else {
+            printError();
+            return null;
+        }
+    }
+
+    public FunctionFeaturesBuilder setIntervals(final Pair<Double, Double> interval) {
+        if (!builded.booleanValue() && this.interval.isEmpty()) {
+            this.interval = Optional.of(interval);
+            return this;
+        } else {
+            printError();
+            return null;
+        }
+    }
+
+    public FunctionFeaturesBuilder setRate(final Double rate) {
+        if (!builded.booleanValue() && this.rate.isEmpty()) {
+        this.rate = Optional.of(rate);
+        return this;
+        } else {
+            printError();
+            return null;
+        }
+    }
+
+    public FunctionFeaturesImpl build() {
+        if (ready()) {
+            return new FunctionFeaturesImpl(function.get(), interval.get(), rate.get());
+        } else {
+            printError();
+            return null;
+        }
+    }
+
 }
