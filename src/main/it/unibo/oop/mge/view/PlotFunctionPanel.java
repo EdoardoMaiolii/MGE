68fac00fe55a09e10573318a46243a6baf5786cd
Elia <elia.urbini@studio.unibo.it>
2020-05-04 10:58:51 +0200

    change PlotFunctionPanel paintComponent

--- src/main/it/unibo/oop/mge/view/PlotFunctionPanel.java
+++ src/main/it/unibo/oop/mge/view/PlotFunctionPanel.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 import it.unibo.oop.mge.c3d.geometry.Point2D;
 import it.unibo.oop.mge.c3d.geometry.Segment2D;
 
-public final class PlotFunctionPanel extends JPanel {
+public class PlotFunctionPanel extends JPanel {
 
     /**
      * 
@@ -19,13 +19,15 @@ public final class PlotFunctionPanel extends JPanel {
     private final List<Segment2D> segments;
 
     public PlotFunctionPanel(final int size, final List<Segment2D> segments) {
+        super();
         this.segments = segments;
         this.center = Point2D.fromDoubles(size / 2, size / 2);
         this.setPreferredSize(new Dimension(size, size));
     }
 
     @Override
-    public void paintComponent(final Graphics g) {
+    public final void paintComponent(final Graphics g) {
+        super.paintComponent(g);
         for (final Segment2D segment : this.segments) {
             g.drawLine((int) (segment.getA().getX() * this.center.getX() + this.center.getX()), (int) (segment.getA().getY() * -this.center.getY() + this.center.getY()),
                     (int) (segment.getB().getX() * this.center.getX() + this.center.getX()), (int) (segment.getB().getY() * -this.center.getY() + this.center.getY()));
