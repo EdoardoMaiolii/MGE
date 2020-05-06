167149f100fa01daccf287ed465f11e590745724
lorib <lori.barto@live.it>
2020-05-06 11:18:26 +0200

    preparation for caching

--- src/main/it/unibo/oop/mge/c3d/MeshDrawerImpl.java
+++ src/main/it/unibo/oop/mge/c3d/MeshDrawerImpl.java
@@ -1,6 +1,7 @@
 package it.unibo.oop.mge.c3d;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import it.unibo.oop.mge.c3d.geometry.Point2D;
@@ -9,29 +10,26 @@ import it.unibo.oop.mge.c3d.geometry.Segment2D;
 
 public class MeshDrawerImpl implements MeshDrawer {
     private final List<Mesh> meshes;
-    private final double rotationXY;
-    private final double rotationYZ;
-    private final Point3D translation;
     private static double targetMeshScale = 100;
     private static Point2D pointOfView = Point2D.fromDoubles(0, -2 * targetMeshScale);
+    private final Function<Point3D, Point2D> renderPoint;
 
     MeshDrawerImpl(final List<Mesh> meshes, final double rotationXY, final double rotationYZ,
             final Point3D translation) {
+
         super();
         final double scale = meshes.stream().mapToDouble(mesh -> mesh.getScale()).max().orElse(1);
         this.meshes = meshes.stream().map((Mesh mesh) -> mesh.transformed(value -> value / scale * targetMeshScale))
                 .collect(Collectors.toList());
-        this.rotationXY = rotationXY;
-        this.rotationYZ = rotationYZ;
-        this.translation = translation;
+        this.renderPoint = (point) -> Point3DRenderer
+                .fromPoint(point.rotated(rotationXY, rotationYZ).translated(translation)).render(pointOfView);
     }
 
     @Override
     public final Mesh2D render() {
         return Mesh2D.of(this.meshes.stream().flatMap(mesh -> mesh.getSegments().stream())
-                .map(seg -> seg.rotated(this.rotationXY, this.rotationYZ)).map(seg -> seg.translated(this.translation))
-                .map(seg -> Segment2D.fromPoints(Point3DRenderer.fromPoint(seg.getA()).render(pointOfView),
-                        Point3DRenderer.fromPoint(seg.getB()).render(pointOfView), seg.getColor()))
+                .map(seg -> Segment2D.fromPoints(renderPoint.apply(seg.getA()), renderPoint.apply(seg.getB()),
+                        seg.getColor()))
                 .map((Segment2D seg) -> seg.transformed(coord -> coord / targetMeshScale))
                 .collect(Collectors.toList()));
 
