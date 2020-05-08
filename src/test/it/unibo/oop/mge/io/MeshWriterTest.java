70e4d1577ec9b374936244b9b2239d8c0d1e4332
lorib <lori.barto@live.it>
2020-05-08 14:17:01 +0200

    javadoc, minor fixes

--- src/test/it/unibo/oop/mge/io/MeshWriterTest.java
+++ src/test/it/unibo/oop/mge/io/MeshWriterTest.java
@@ -14,29 +14,29 @@ import java.io.IOException;
 
 class MeshWriterTest {
 
-    Point3D a = Point3D.fromDoubles(-100, -100, 0);
-    Point3D b = Point3D.fromDoubles(100, -100, 0);
-    Point3D c = Point3D.fromDoubles(-100, 100, 0);
-    Point3D d = Point3D.fromDoubles(100, 100, 0);
+    private final Point3D a = Point3D.fromDoubles(-100, -100, 0);
+    private final Point3D b = Point3D.fromDoubles(100, -100, 0);
+    private final Point3D c = Point3D.fromDoubles(-100, 100, 0);
+    private final Point3D d = Point3D.fromDoubles(100, 100, 0);
 
-    Segment3D ab = Segment3D.fromPoints(a, b, Color.RED);
-    Segment3D bd = Segment3D.fromPoints(b, d);
-    Segment3D cd = Segment3D.fromPoints(c, d);
-    Segment3D ca = Segment3D.fromPoints(c, a);
+    private final Segment3D ab = Segment3D.fromPoints(a, b, Color.RED);
+    private final Segment3D bd = Segment3D.fromPoints(b, d);
+    private final Segment3D cd = Segment3D.fromPoints(c, d);
+    private final Segment3D ca = Segment3D.fromPoints(c, a);
 
-    List<Segment3D> set = Arrays.asList(ab, bd, cd, ca);
+    private final List<Segment3D> set = Arrays.asList(ab, bd, cd, ca);
 
-    Mesh mesh = Mesh.fromSegments(set);
+    private final Mesh mesh = Mesh.fromSegments(set);
 
     @Test
-    void test() throws IOException {
+    public void test() throws IOException {
 
-        MeshWriter writer = MeshWriter.fromMesh(mesh);
+        final MeshWriter writer = MeshWriter.fromMesh(mesh);
         writer.write("C:\\users\\eliau\\desktop\\test.yml");
     }
 
     @Test
-    void test2() {
+    public void test2() {
         System.out.println(MeshYamlRepresentation.of(mesh).toYamlString());
     }
 }
