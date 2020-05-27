6ac57c40edab4b0f7727ac41df36809a487c2c5f
kevin mancini <kevin.mancini@studio.unibo.it>
2020-05-27 15:06:39 +0200

    add javadoc

--- src/main/it/unibo/oop/mge/model/FunctionFeaturesBuilder.java
+++ src/main/it/unibo/oop/mge/model/FunctionFeaturesBuilder.java
@@ -4,21 +4,54 @@ import java.awt.Color;
 
 import it.unibo.oop.mge.color.VariableColor;
 import it.unibo.oop.mge.function.AlgebricFunction;
-import it.unibo.oop.mge.libraries.Pair;
 
 public interface FunctionFeaturesBuilder {
-
+    /**
+     * 
+     * @param function
+     * @return a FunctionFeaturesBuilder
+     */
     FunctionFeaturesBuilder setFunction(AlgebricFunction function);
 
+    /**
+     * 
+     * @param min
+     * @param max
+     * @return a FunctionFeaturesBuilder
+     */
     FunctionFeaturesBuilder setIntervals(double min, double max);
 
+    /**
+     * 
+     * @param rate
+     * @return a FunctionFeaturesBuilder
+     */
     FunctionFeaturesBuilder setRate(double rate);
 
+    /**
+     * 
+     * @param varColor
+     * @return a FunctionFeaturesBuilder
+     */
     FunctionFeaturesBuilder setDinamicColor(VariableColor varColor);
 
+    /**
+     * 
+     * @param color
+     * @return a FunctionFeaturesBuilder
+     */
     FunctionFeaturesBuilder setStaticColor(Color color);
 
+    /**
+     * 
+     * @param decimalPrecision
+     * @return a FunctionFeaturesBuilder
+     */
     FunctionFeaturesBuilder setDecimalPrecision(int decimalPrecision);
 
+    /**
+     * 
+     * @return a FunctionFeaturesImpl with all the setting given.
+     */
     FunctionFeaturesImpl build();
 }
