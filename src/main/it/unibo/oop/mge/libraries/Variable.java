090cd513274edf13b3be38f3ca9d86e98b48e2c2
kevin mancini <kevin.mancini@studio.unibo.it>
2020-05-27 16:14:08 +0200

    fix name

--- src/main/it/unibo/oop/mge/libraries/Variable.java
+++ src/main/it/unibo/oop/mge/libraries/Variable.java
@@ -1,11 +1,8 @@
 package it.unibo.oop.mge.libraries;
 
-import java.util.EnumSet;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-public enum Variable {
+public enum Variable implements GenericEnum {
 
     X("x"), Y("y");
 
@@ -20,10 +17,10 @@ public enum Variable {
     }
 
     public static List<String> getListFromEnum() {
-        return EnumSet.allOf(Variable.class).stream().map(i -> i.getSyntax()).collect(Collectors.toList());
+        return EnumUtilityImpl.getSyntaxList(MathFunction.class);
     }
 
-    public static Optional<Variable> getConstantFromSyntax(final String syntax) {
-        return EnumSet.allOf(Variable.class).stream().filter(i -> i.getSyntax() == syntax).findFirst();
+    public static Variable getVariableFromSyntax(final String syntax) {
+        return (Variable) EnumUtilityImpl.getElement(Variable.class, syntax);
     }
 }
