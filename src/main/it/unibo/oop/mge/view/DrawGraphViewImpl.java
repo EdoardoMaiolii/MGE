df73b3991e2798603e521dabf8cf5aa3c301027c
Elia <elia.urbini@studio.unibo.it>
2020-06-10 17:48:25 +0200

    fix GUI bug

--- src/main/it/unibo/oop/mge/view/DrawGraphViewImpl.java
+++ src/main/it/unibo/oop/mge/view/DrawGraphViewImpl.java
@@ -345,6 +345,7 @@ public class DrawGraphViewImpl implements DrawGraphView {
                 this.properties.get(this.properties.indexOf(next)).setText(property);
             }
         }
+        inputFrame.pack();
     }
 
     /**
