83053b398e3bbe57a7d07b8663096811f67a243e
Elia <elia.urbini@studio.unibo.it>
2020-06-15 10:44:19 +0200

    add meshLoader test

--- src/test/it/unibo/oop/mge/io/MeshWriterAndLoaderTest.java
+++ src/test/it/unibo/oop/mge/io/MeshWriterAndLoaderTest.java
@@ -9,10 +9,14 @@ import it.unibo.oop.mge.c3d.Mesh;
 import it.unibo.oop.mge.c3d.geometry.Point3D;
 import it.unibo.oop.mge.c3d.geometry.Segment3D;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-class MeshWriterTest {
+class MeshWriterAndLoaderTest {
 
     private final Point3D a = Point3D.fromDoubles(-100, -100, 0);
     private final Point3D b = Point3D.fromDoubles(100, -100, 0);
@@ -25,14 +29,21 @@ class MeshWriterTest {
     private final Segment3D ca = Segment3D.fromPoints(c, a);
 
     private final List<Segment3D> set = Arrays.asList(ab, bd, cd, ca);
-
     private final Mesh mesh = Mesh.fromSegments(set);
+    private static final String SEP = File.separator; 
+    private static final String PATH = System.getProperty("user.home") + SEP + "desktop" + SEP + "test.yml"; 
 
     @Test
-    public void test() throws IOException {
-
+    public void testWriter() throws IOException {
         final MeshWriter writer = MeshWriter.fromMesh(mesh);
-        writer.write("C:\\users\\eliau\\desktop\\test.yml");
+        writer.write(PATH);
+    }
+
+    @Test
+    public void testLoader() throws FileNotFoundException, IOException {
+        final MeshLoader loader = new MeshLoaderImpl();
+        final Mesh mesh = loader.load(PATH);
+        //assertEquals(this.mesh, mesh);
     }
 
     @Test
